ae9c354de038f5f935ec2888cbc008ba412809a0
Nnomenja <mamenosoa42@gmail.com>
2026-06-15 17:45:50 +0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Estate",
+    "version": "1.0",
+    "summary": "Module simple pour gérer des notes",
+    "author": "Mamenosoa",
+    "category": "Tutorial",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tags_views.xml",
+        "views/estate_menus.xml",
+    ],
+    "installable": True,
+    "application": True,
+}   
---
+++ models/estate_property.py
@@ -0,0 +1,33 @@
+from odoo import models, fields
+from dateutil.relativedelta import relativedelta
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Estate Property"
+
+    name = fields.Char(required=True, string="Title")
+    description = fields.Text(string="Description")
+    postcode = fields.Char()
+    date_availability = fields.Date(default=lambda self: fields.Date.today() + relativedelta(months=3), string="Available From" )
+    expected_price = fields.Float(required=True, string="Expected Price")
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2, string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean()
+    garden = fields.Boolean()   
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[('North', 'North'), ('South', 'South'), ('East', 'East'), ('West', 'West')]
+    )
+    active = fields.Boolean(default=False)
+    state = fields.Selection(
+        string="Status",
+        default="New",
+        required=True,
+        selection=[('New', 'New'), ('Offer Received', 'Offer Received'), ('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'), ('Cancelled', 'Cancelled')]
+    )
+    property_type_id = fields.Many2one(comodel_name="estate.property.type", string="Property Type")
+    property_tags_id = fields.Many2many(comodel_name="estate.property.tags", string="Property Tags")
+    salesman_id = fields.Many2one(comodel_name="res.users", default=lambda self: self.env.user, string="Salesman")
+    buyer_id = fields.Many2one(comodel_name="res.partner", string="Buyer")
